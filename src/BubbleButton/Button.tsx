74e235d7f3b736be43634fcde9a5d63995d680bf
Marcobetancourt9 <marco.betancourt@correo.unimet.edu.ve>
2026-02-18 21:50:25 -0400

    solo unimet

--- src/BubbleButton/Button.tsx
+++ src/BubbleButton/Button.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaUsers } from 'react-icons/fa';
+import { useAuth } from "@/contexts/AuthContext";
 import './Button.css';
 
 // Definir los tipos para las props (aunque este componente no recibe props)
 interface CombinedBubbleProps { }
 
 const CombinedBubble: React.FC<CombinedBubbleProps> = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const { user } = useAuth();
+
+  useEffect(() => {
+    // Verificar si el usuario tiene correo de la universidad
+    if (user?.email && user.email.endsWith('@correo.unimet.edu.ve')) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  }, [user]); // Se ejecuta cada vez que cambia el usuario
+
   const handleWhatsAppClick = (): void => {
     window.open('https://wa.me/+584125936487', '_blank');
   };
+
+  // Si no es visible, no renderizar nada
+  if (!isVisible) {
+    return null;
+  }
 
   return (
     <div
